5f92fbec06b3461b647782dc98ad168b60edda12
mrumiker <69427808+mrumiker@users.noreply.github.com>
2022-03-29 22:41:15 -0500

    Initial Commit

---
+++ src/components/TitlePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TitlePage() {
+  return (<div className="title-page-container">
+    <div className='game--title'>Quizzical: Music Edition</div>
+    <div className='game--description'>Test Your Tunes</div>
+    <button className='game--start-button'>Start Quiz</button>
+  </div>);
+}
