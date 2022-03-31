f7b916138a4e5a5ae6e010143a85b4eb0b5d8ccf
mrumiker <69427808+mrumiker@users.noreply.github.com>
2022-03-31 15:49:59 -0500

    create Answer hover effect

--- src/components/TriviaGame.js
+++ src/components/TriviaGame.js
@@ -23,8 +23,9 @@ export default function TriviaGame(props) {
           }
           return {
             question,
-            correctAnswer,
             answers,
+            correctAnswer,
+            selectedAnswer: null,
           }
         });
         setQuestions(questionData);
