cd63db1a60e2e6a0b94d5d08c9741ba702223fe4
mrumiker <69427808+mrumiker@users.noreply.github.com>
2022-03-30 22:45:32 -0500

    get Trivia from API

--- src/components/TriviaGame.js
+++ src/components/TriviaGame.js
@@ -1,22 +1,34 @@
 import React from "react";
-// import mockData from "../mockData.json";
 
 export default function TriviaGame(props) {
+
   const [questions, setQuestions] = React.useState([]);
 
   React.useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple")
       .then(res => res.json())
-      .then(data => setQuestions(data));
+      .then(data => setQuestions(data.results))
+      .catch(err => console.error(err));
   }, []);
+
+  console.log("Questions =", questions);
+
+  const questionsArr = questions.length ?
+    questions.map((questionData, i) => (
+      <div key={i}>
+        <h2>{questionData.question}</h2>
+        <p>Answer: {questionData.correct_answer}</p>
+        <hr />
+      </div>
+    )) :
+    [];
 
   return (
     <div>
-      <h1>GAME</h1>
+      <h1>Your Questions</h1>
+      <hr />
+      {questionsArr}
       <button className="game--start-button" onClick={props.handleClick}>Go Back</button>
-
-      {JSON.stringify(questions)}
     </div>
-
   )
 }
